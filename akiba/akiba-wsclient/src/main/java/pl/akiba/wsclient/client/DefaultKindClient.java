35c8400c70aac3f5a6875aa522aad25842806f38
OstroS <ostros@gmail.com>
2013-03-18 17:47:31 +0100

    Go

--- akiba/akiba-wsclient/src/main/java/pl/akiba/wsclient/client/DefaultKindClient.java
+++ akiba/akiba-wsclient/src/main/java/pl/akiba/wsclient/client/DefaultKindClient.java
@@ -1,12 +1,16 @@
 package pl.akiba.wsclient.client;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.eclipse.jetty.client.ContentExchange;
 import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.HttpExchange;
 
 import pl.akiba.model.entities.Kind;
-import pl.akiba.model.exception.EmptyResultException;
-import pl.akiba.model.exception.EntityIsNotValidException;
+import pl.akiba.model.exception.MethodFailureStatusException;
+import pl.akiba.model.exception.NotFoundStatusException;
+import pl.akiba.model.exception.StatusException;
 import pl.akiba.model.service.KindService;
 
 /**
@@ -20,33 +24,113 @@ public class DefaultKindClient extends DefaultClient implements KindService {
     }
 
     @Override
-    public Kind get(int userId, int kindId) throws EmptyResultException {
-        // TODO Auto-generated method stub
-        return null;
+    public Kind get(int userId, int kindId) throws StatusException, IOException, InterruptedException {
+        StringBuilder urlBuilder = prepareBasicUrl(userId).append("/kind/").append(kindId);
+
+        ContentExchange exchange = sendExchange(HttpMethod.GET, urlBuilder.toString(), null);
+        int exchangeStatus = exchange.waitForDone();
+
+        if (exchangeStatus == HttpExchange.STATUS_COMPLETED) {
+            int responseStatus = exchange.getResponseStatus();
+            switch (responseStatus) {
+                case 200:
+                    return mapper.readValue(exchange.getResponseContent(), Kind.class);
+                case 404:
+                    throw new NotFoundStatusException("Http response returns NOT FOUND (404) status");
+                case 420:
+                    throw new MethodFailureStatusException("Http response returns METHOD FAILURE (420) status");
+                default:
+                    throw new StatusException("Http response returns unknown (" + responseStatus + ") status");
+            }
+        }
+
+        throw new StatusException("Http exchange returns status: " + getExchangeStatusName(exchangeStatus));
     }
 
     @Override
-    public List<Kind> getAll(int userId) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Kind> getAll(int userId) throws StatusException, IOException, InterruptedException {
+        StringBuilder urlBuilder = prepareBasicUrl(userId).append("/kind");
+
+        ContentExchange exchange = sendExchange(HttpMethod.GET, urlBuilder.toString(), null);
+        int exchangeStatus = exchange.waitForDone();
+
+        if (exchangeStatus == HttpExchange.STATUS_COMPLETED) {
+            int responseStatus = exchange.getResponseStatus();
+            switch (responseStatus) {
+                case 200:
+                    return mapJsonToEntities(exchange.getResponseContent(), Kind.class);
+                case 404:
+                    throw new NotFoundStatusException("Http response returns NOT FOUND (404) status");
+                case 420:
+                    throw new MethodFailureStatusException("Http response returns METHOD FAILURE (420) status");
+                default:
+                    throw new StatusException("Http response returns unknown (" + responseStatus + ") status");
+            }
+        }
+
+        throw new StatusException("Http exchange returns status: " + getExchangeStatusName(exchangeStatus));
     }
 
     @Override
-    public Kind create(int userId, Kind kind) throws EntityIsNotValidException {
-        // TODO Auto-generated method stub
-        return null;
+    public Kind create(int userId, Kind kind) throws StatusException, IOException, InterruptedException {
+        StringBuilder urlBuilder = prepareBasicUrl(userId).append("/kind");
+
+        ContentExchange exchange = sendExchange(HttpMethod.POST, urlBuilder.toString(), kind);
+        int exchangeStatus = exchange.waitForDone();
+
+        if (exchangeStatus == HttpExchange.STATUS_COMPLETED) {
+            int responseStatus = exchange.getResponseStatus();
+            switch (responseStatus) {
+                case 201:
+                    return mapper.readValue(exchange.getResponseContent(), Kind.class);
+                case 420:
+                    throw new MethodFailureStatusException("Http response returns METHOD FAILURE (420) status");
+                default:
+                    throw new StatusException("Http response returns unknown (" + responseStatus + ") status");
+            }
+        }
+
+        throw new StatusException("Http exchange returns status: " + getExchangeStatusName(exchangeStatus));
     }
 
     @Override
-    public void update(int userId, Kind kind) throws EntityIsNotValidException {
-        // TODO Auto-generated method stub
+    public void update(int userId, Kind kind) throws StatusException, IOException, InterruptedException {
+        StringBuilder urlBuilder = prepareBasicUrl(userId).append("/kind");
 
+        ContentExchange exchange = sendExchange(HttpMethod.PUT, urlBuilder.toString(), kind);
+        int exchangeStatus = exchange.waitForDone();
+
+        if (exchangeStatus == HttpExchange.STATUS_COMPLETED) {
+            int responseStatus = exchange.getResponseStatus();
+            switch (responseStatus) {
+                case 420:
+                    throw new MethodFailureStatusException("Http response returns METHOD FAILURE (420) status");
+                default:
+                    throw new StatusException("Http response returns unknown (" + responseStatus + ") status");
+            }
+        }
+
+        throw new StatusException("Http exchange returns status: " + getExchangeStatusName(exchangeStatus));
     }
 
     @Override
-    public void delete(int userId, int kindId) {
-        // TODO Auto-generated method stub
+    public void delete(int userId, int kindId) throws StatusException, IOException, InterruptedException {
+        StringBuilder urlBuilder = prepareBasicUrl(userId).append("/kind/").append(kindId);
 
+        ContentExchange exchange = sendExchange(HttpMethod.DELETE, urlBuilder.toString(), null);
+        int exchangeStatus = exchange.waitForDone();
+
+        if (exchangeStatus == HttpExchange.STATUS_COMPLETED) {
+            int responseStatus = exchange.getResponseStatus();
+            switch (responseStatus) {
+                case 420:
+                    throw new MethodFailureStatusException("Http response returns METHOD FAILURE (420) status");
+                default:
+                    throw new StatusException("Http response returns unknown (" + responseStatus + ") status");
+            }
+        }
+
+        throw new StatusException("Http exchange returns status: " + getExchangeStatusName(exchangeStatus));
     }
 
 }
