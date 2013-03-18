e302bedfd5ff381833cf6549d166b7dab99bc0d2
OstroS <ostros@gmail.com>
2013-03-18 17:47:46 +0100

    Go2

--- akiba/akiba-model/src/main/java/pl/akiba/model/service/ProfileService.java
+++ akiba/akiba-model/src/main/java/pl/akiba/model/service/ProfileService.java
@@ -1,10 +1,12 @@
 package pl.akiba.model.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import pl.akiba.model.entities.Profile;
 import pl.akiba.model.exception.EmptyResultException;
 import pl.akiba.model.exception.EntityIsNotValidException;
+import pl.akiba.model.exception.StatusException;
 
 /**
  * 
@@ -12,14 +14,17 @@ import pl.akiba.model.exception.EntityIsNotValidException;
  */
 public interface ProfileService {
 
-    public List<Profile> getAll(int userId);
+    public List<Profile> getAll(int userId) throws StatusException, IOException, InterruptedException;
 
-    public Profile getDefault(int userId) throws EmptyResultException;;
+    public Profile getDefault(int userId) throws EmptyResultException, StatusException, IOException,
+            InterruptedException;
 
-    public Profile create(int userId, Profile profile) throws EntityIsNotValidException;
+    public Profile create(int userId, Profile profile) throws EntityIsNotValidException, StatusException, IOException,
+            InterruptedException;
 
-    public void update(int userId, Profile profile) throws EntityIsNotValidException;
+    public void update(int userId, Profile profile) throws EntityIsNotValidException, StatusException, IOException,
+            InterruptedException;
 
-    public void delete(int userId, int profileId);
+    public void delete(int userId, int profileId) throws StatusException, IOException, InterruptedException;
 
 }
